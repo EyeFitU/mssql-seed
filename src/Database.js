865003efcec0138e4266e50ec2feb2602656d6c0
Pavel Zubkou <pavel.zubkou@gmail.com>
2018-02-05 18:53:49 +0300

    Replace too bread applySchema with createTable

--- src/Database.js
+++ src/Database.js
@@ -1,26 +1,88 @@
 'use strict';
 
 const assert = require('assert');
+const mssql = require('mssql');
 const Promise = require('bluebird');
-const VError = require('verror');
-const readSqlFiles = require('./readSqlFiles');
 const readFile = Promise.promisify(require('fs').readFile);
 
+const BATCH_SPLIT_RE = /\r?\nGO\r?\n/;
+const getStatementsForBatch = (sql) => {
+  // Split statements by GO command, which is not part of T-SQL.
+  // https://github.com/patriksimek/node-mssql/issues/282
+  const statements = sql.split(BATCH_SPLIT_RE);
+
+  // Files could have empty lines, which cause error when passed to batch
+  // function. We call .trim() to prevent "Incorrect syntax near ''." error.
+  return statements.map(s => s.trim());
+};
+
 class Database {
-  constructor({ name, pool }) {
+  constructor({ name, connectionOptions }) {
     assert(name, 'database name should be provided');
-    assert(pool, 'connection pool from mssql package should be passed');
+    assert(connectionOptions, 'connection options for mssql package should be passed');
+    assert(connectionOptions.user);
+    assert(connectionOptions.password);
+    assert(connectionOptions.server);
+    assert(connectionOptions.database);
 
     this.name = name;
-    this.pool = pool;
+    this.connectionOptions = connectionOptions;
   }
 
-  drop() {
-    return this.pool.request().batch(`DROP DATABASE [${this.name}]`);
+  openServerConnection() {
+    if (this.serverConnectionPromise) {
+      return this.serverConnectionPromise;
+    }
+
+    const connectionPool = new mssql.ConnectionPool(Object.assign({}, this.connectionOptions, {
+      // Database name is not passed into connection because we are not sure on
+      // its existence. Setting it while it doesn't exists would result in a
+      // login error.
+      database: undefined,
+    }));
+
+    this.serverConnectionPromise = connectionPool.connect();
+
+    return this.serverConnectionPromise;
   }
 
-  createIfNotExists() {
-    return this.pool.request().batch(`
+  openDatabaseConnection() {
+    if (this.databaseConnectionPromise) {
+      return this.databaseConnectionPromise;
+    }
+
+    const connectionPool = new mssql.ConnectionPool(this.connectionOptions);
+
+    this.databaseConnectionPromise = connectionPool.connect();
+
+    return this.databaseConnectionPromise;
+  }
+
+  async close() {
+    if (this.serverConnectionPromise) {
+      this.serverConnectionPromise.then(pool => pool.close());
+    }
+
+    if (this.databaseConnectionPromise) {
+      this.databaseConnectionPromise.then(pool => pool.close());
+    }
+  }
+
+  async drop() {
+    const connection = await this.openServerConnection();
+
+    return connection.request().batch(`
+      IF db_id('${this.name}') IS NOT NULL
+      BEGIN
+        DROP DATABASE [${this.name}]
+      END
+    `);
+  }
+
+  async create() {
+    const connection = await this.openServerConnection();
+
+    return connection.request().batch(`
       IF db_id('${this.name}') IS NULL
       BEGIN
         CREATE DATABASE [${this.name}]
@@ -28,19 +90,13 @@ class Database {
     `);
   }
 
-  async applySchema({ directory }) {
-    const filelist = await readSqlFiles(directory);
-    const result = await Promise.map(filelist, async (file) => {
-      const multipleSqlStatements = await readFile(file, { encoding: 'utf8' });
+  async createTable({ file }) {
+    const sql = await readFile(file, { encoding: 'utf8' });
+    const connection = await this.openDatabaseConnection();
+    const execute = statement => connection.request().batch(statement);
+    const statements = getStatementsForBatch(sql);
 
-      try {
-        return await this.pool.request().batch(multipleSqlStatements);
-      } catch (err) {
-        throw new VError(err, `Failed to run batch reqeust from ${file}`);
-      }
-    });
-
-    return result;
+    return Promise.each(statements, execute);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,18 +4,6 @@ const assert = require('assert');
 const mssql = require('mssql');
 const Database = require('./Database');
 
-module.exports = async (options) => {
-  assert(options.user);
-  assert(options.password);
-  assert(options.server);
-  assert(options.database);
-
-  const pool = await mssql.connect(Object.assign({}, options, {
-    // Database name is not passed into connection because we are not sure on
-    // its existence. Setting it while it doesn't exists would result in a
-    // login error.
-    database: undefined,
-  }));
-
-  return new Database({ name: options.database, pool });
+module.exports = (options) => {
+  return new Database({ name: options.database, connectionOptions: options });
 };
